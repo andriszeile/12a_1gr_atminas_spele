cf070a41585d366018b98359093c6ec5cc1bd9d3
andriszeile <71541542+andriszeile@users.noreply.github.com>
2025-01-20 09:56:33 +0200

    11

--- static/top.js
+++ static/top.js
@@ -1,51 +1,58 @@
-//no URL iegūst vārdu un ievieto to virsrakstā
 let adrese = window.location.hash;
 adrese = decodeURI(adrese);
-adrese = adrese.replace('#','');
+adrese = adrese.replace('#', '');
 adrese = adrese.split(",");
-vards  = adrese[0];
-klikski = adrese[1];
-laiks = adrese[2];
+let vards = adrese[0];
+let klikski = adrese[1];
+let laiks = adrese[2];
 
 let datums = new Date();
-let datumsVirkne = datums.getDate()+'.'+datums.getMonth()+'.'+datums.getFullYear()+'.'
+let datumsVirkne = datums.getDate() + '.' + (datums.getMonth() + 1) + '.' + datums.getFullYear() + '.';
 
-async function iegutDatusNoApi(url)
-{
-  let datiNoServera = await fetch(url);
-  let datiNoServeraJson = await datiNoServera.json();
-  return datiNoServeraJson;
+async function iegutDatusNoApi(url) {
+    try {
+        let datiNoServera = await fetch(url);
+        if (!datiNoServera.ok) {
+            throw new Error("Servera atbilde nav veiksmīga: " + datiNoServera.status);
+        }
+        let datiNoServeraJson = await datiNoServera.json();
+        return datiNoServeraJson;
+    } catch (error) {
+        console.error("Kļūda iegūstot datus no API:", error);
+        return [];
+    }
 }
 
-async function atlasitTop()
-{
-  let topsJson = await iegutDatusNoApi('./result.json');
-
-  console.log(datiJson);
-  for (i=0; i < topsJson.length; i++)
-  {
+async function atlasitTop() {
+    let topsJson = await iegutDatusNoApi('../result.json'); // Pārliecinies, ka ceļš ir pareizs.
     let tabula = document.querySelector(".tops");
 
-    tabula.innerHTML = tabula.innerHTML+`
-    <tr id="`+topsJson[i]['id']+`">
-      <td> `+topsJson[i]['vards']+` </td>
-      <td> `+topsJson[i]['klikski']+` </td>
-      <td> `+topsJson[i]['laiks']+` </td>
-      <td> `+topsJson[i]['datums']+` </td>
-    </tr>`;
-  }
+    topsJson.forEach((item) => {
+        let rinda = document.createElement('tr');
+        rinda.id = item['id'];
+
+        rinda.innerHTML = `
+            <td>${item['vards']}</td>
+            <td>${item['klikski']}</td>
+            <td>${item['laiks']}</td>
+            <td>${item['datums']}</td>
+        `;
+        tabula.appendChild(rinda);
+    });
 }
 
 atlasitTop();
 
-
 function pievienotTop() {
-  let tabula = document.querySelector('.tops');
-  tabula.innerHTML = tabula.innerHTML +`
-    <tr id='jauns'>
-      <td>`+vards+`</td>
-      <td>`+klikski+`</td>
-      <td>`+laiks+`</td>
-      <td>`+datumsVirkne+`</td>
-    </tr>`;
+    let tabula = document.querySelector('.tops');
+    let rinda = document.createElement('tr');
+    rinda.id = 'jauns';
+
+    rinda.innerHTML = `
+        <td>${vards}</td>
+        <td>${klikski}</td>
+        <td>${laiks}</td>
+        <td>${datumsVirkne}</td>
+    `;
+    tabula.appendChild(rinda);
 }
